70b1444012def13aaf66de77546530bb4ea386f0
Anna Malysheva <milva@mail.ru>
2013-04-03 18:15:18 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trillian::Application.config.secret_token = '98a30659e72290b76311f0039cea2ccd508277f7a9468f01a20dfe8d8d2d1cad818bf09ad17285a91bcf422a2b8a2b3097d5101aa5b827020cddf01e6f014d8f'
